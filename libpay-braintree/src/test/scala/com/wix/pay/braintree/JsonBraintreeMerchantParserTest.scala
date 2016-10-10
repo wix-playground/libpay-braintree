b83b3083f7f1439ef2b1f5eb4418fac266979382
Nimrod Lahav <nimrodl@wix.com>
2016-10-10 11:57:59 +0300

    move to 1 merchant parser
    add IT test for OAuth merchant

--- libpay-braintree/src/test/scala/com/wix/pay/braintree/JsonBraintreeMerchantParserTest.scala
+++ libpay-braintree/src/test/scala/com/wix/pay/braintree/JsonBraintreeMerchantParserTest.scala
@@ -6,24 +6,38 @@ import org.specs2.specification.Scope
 
 
 class JsonBraintreeMerchantParserTest extends SpecWithJUnit {
+
   trait Ctx extends Scope {
     val merchantParser: BraintreeMerchantParser = new JsonBraintreeMerchantParser
   }
 
+  val someMerchantAccountId = Some(Map(
+    "USD" -> "some merchant account ID",
+    "EUR" -> "some other merchant account ID"
+  ))
+
   "stringify and then parse" should {
-    "yield a merchant similar to the original one" in new Ctx {
+
+    "yield a regular Braintree merchant similar to the original one" in new Ctx {
       val someMerchant = BraintreeMerchant(
         merchantId = "some merchant ID",
         publicKey = "some public key",
         privateKey = "some private key",
-        merchantAccountIds = Some(Map(
-          "USD" -> "some merchant account ID",
-          "EUR" -> "some other merchant account ID"
-        ))
+        merchantAccountIds = someMerchantAccountId
       )
 
       val merchantKey = merchantParser.stringify(someMerchant)
       merchantParser.parse(merchantKey) must beEqualTo(someMerchant)
+    }
+
+    "yield a OAuth Braintree merchant similar to the original one" in new Ctx {
+      val someOAuthMerchant = BraintreeOAuthMerchant(
+        token = "some token",
+        merchantAccountIds = someMerchantAccountId
+      )
+
+      val merchantKey = merchantParser.stringify(someOAuthMerchant)
+      merchantParser.parse(merchantKey) must beEqualTo(someOAuthMerchant)
     }
   }
 }
