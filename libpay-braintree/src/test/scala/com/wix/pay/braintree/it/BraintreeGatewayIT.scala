bf3d497737fd3993c952c9364285147f5c9b7ddb
Raz, Ohad <ohadr@wix.com>
2017-12-12 15:10:08 +0200

    no more spray

--- libpay-braintree/src/test/scala/com/wix/pay/braintree/it/BraintreeGatewayIT.scala
+++ libpay-braintree/src/test/scala/com/wix/pay/braintree/it/BraintreeGatewayIT.scala
@@ -1,15 +1,15 @@
 package com.wix.pay.braintree.it
 
 
+import org.specs2.mutable.SpecWithJUnit
+import org.specs2.specification.Scope
+import akka.http.scaladsl.model.StatusCodes
 import com.braintreegateway.Environment
 import com.wix.pay.braintree._
 import com.wix.pay.braintree.testkit.BraintreeDriver
 import com.wix.pay.creditcard.{CreditCard, CreditCardOptionalFields, YearMonth}
 import com.wix.pay.model.{CurrencyAmount, Payment}
 import com.wix.pay.{PaymentErrorException, PaymentGateway, PaymentRejectedException}
-import org.specs2.mutable.SpecWithJUnit
-import org.specs2.specification.Scope
-import spray.http.StatusCodes
 
 
 class BraintreeGatewayIT extends SpecWithJUnit {
@@ -21,12 +21,12 @@ class BraintreeGatewayIT extends SpecWithJUnit {
 
   val someChannel = "someChannel"
 
-  val someMerchant = new BraintreeMerchant(
+  val someMerchant = BraintreeMerchant(
     merchantId = "someMerchantID",
     publicKey = "somePublicKey",
     privateKey = "somePrivateKey",
     merchantAccountIds = Map("USD" -> "someMerchantAccountID"))
-  val merchantKey = merchantParser.stringify(someMerchant)
+  val merchantKey: String = merchantParser.stringify(someMerchant)
   val someCurrencyAmount = CurrencyAmount("USD", 33.3)
   val somePayment = Payment(someCurrencyAmount, 1)
   val someCreditCard = CreditCard(
@@ -38,8 +38,9 @@ class BraintreeGatewayIT extends SpecWithJUnit {
       billingAddress = Some("some billing address"),
       billingPostalCode = Some("90210"))))
 
+
   step {
-    driver.startProbe()
+    driver.start()
   }
 
   sequential
@@ -53,8 +54,9 @@ class BraintreeGatewayIT extends SpecWithJUnit {
       channel = Some(someChannel)
     )
 
-    driver.resetProbe()
+    driver.reset()
   }
+
 
   "sale request via Braintree gateway" should {
     "gracefully fail on invalid card number" in new Ctx {
@@ -63,18 +65,15 @@ class BraintreeGatewayIT extends SpecWithJUnit {
         someMerchant.publicKey,
         someMerchant.privateKey,
         someCurrencyAmount,
-        someCreditCard
-      ) failsOnInvalidCardNumber()
+        someCreditCard) failsOnInvalidCardNumber()
 
       braintree.sale(
         merchantKey = merchantKey,
         creditCard = someCreditCard,
-        payment = somePayment
-      ) must beAFailedTry(
-        check = beAnInstanceOf[PaymentRejectedException]
-      )
+        payment = somePayment) must beAFailedTry(check = beAnInstanceOf[PaymentRejectedException])
     }
   }
+
 
   "authorize request via Braintree gateway" should {
     "gracefully fail on invalid merchant key" in new Ctx {
@@ -88,15 +87,12 @@ class BraintreeGatewayIT extends SpecWithJUnit {
       braintree.authorize(
         merchantKey = merchantKey,
         creditCard = someCreditCard,
-        payment = somePayment
-      ) must beAFailedTry(
-        check = beAnInstanceOf[PaymentErrorException]
-      )
+        payment = somePayment) must beAFailedTry(check = beAnInstanceOf[PaymentErrorException])
     }
 
     "successfully yield an authorization key on valid request" in new Ctx {
       val someTransactionId = "someTransactionID"
-      val authorizationKey = authorizationParser.stringify(BraintreeAuthorization(someTransactionId))
+      val authorizationKey: String = authorizationParser.stringify(BraintreeAuthorization(someTransactionId))
 
       driver.aCreateSaleRequestFor(
         someMerchant.merchantId,
@@ -108,10 +104,7 @@ class BraintreeGatewayIT extends SpecWithJUnit {
       braintree.authorize(
         merchantKey = merchantKey,
         creditCard = someCreditCard,
-        payment = somePayment
-      ) must beASuccessfulTry(
-        check = ===(authorizationKey)
-      )
+        payment = somePayment) must beASuccessfulTry(check = ===(authorizationKey))
     }
 
     "gracefully fail on rejected payment" in new Ctx {
@@ -125,14 +118,12 @@ class BraintreeGatewayIT extends SpecWithJUnit {
       braintree.authorize(
         merchantKey = merchantKey,
         creditCard = someCreditCard,
-        payment = somePayment
-      ) must beAFailedTry(
-        check = beAnInstanceOf[PaymentRejectedException]
-      )
+        payment = somePayment) must beAFailedTry(check = beAnInstanceOf[PaymentRejectedException])
     }
   }
 
+
   step {
-    driver.stopProbe()
+    driver.stop()
   }
 }
